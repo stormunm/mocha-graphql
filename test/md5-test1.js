98b40e028c25ff9968c56f437573d22e99d24f73
Michael Angerman <stormasm@gmail.com>
2017-06-17 19:53:18 -0700

    cc

--- test/md5-test1.js
+++ test/md5-test1.js
@@ -18,8 +18,6 @@ import { md5 } from "./../js/md5.js";
 describe("MD5 Hex-encoding", () => {
   it("should create a hex-encoded MD5 hash of an ASCII value", async () => {
     const result = await md5("value");
-    //const result = await 1
-    //expect(1).to.equal(result);
     expect(result).to.equal("2063c1608d6e0baf80249c42e2be5804");
   });
 });
