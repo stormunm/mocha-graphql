785a3bc5b68d8f9d0492958be0cbff394bad5588
Michael Angerman <stormasm@gmail.com>
2017-06-17 17:34:41 -0700

    working on md5 testing

--- build_alias.sh
+++ build_alias.sh
@@ -5,3 +5,5 @@ alias sw2='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require .
 alias sw3='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require ./resources/mocha-bootload --check-leaks --full-trace ./test/starWarsValidation-test.js'
 
 alias swall='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require ./resources/mocha-bootload --check-leaks --full-trace ./test/*-test.js'
+
+alias swmd5='./node_modules/.bin/babel-node ./node_modules/.bin/_mocha --require ./resources/mocha-bootload --check-leaks --full-trace ./test/md5-test.js'
