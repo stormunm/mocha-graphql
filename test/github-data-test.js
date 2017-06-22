acf08984f77eebcaed4af9715cee3358fa863231
Michael Angerman <stormasm@gmail.com>
2017-06-21 21:28:42 -0700

    testing related topics

--- test/github-data-test.js
+++ test/github-data-test.js
@@ -11,11 +11,22 @@ describe("read topic map", function() {
   });
 });
 
-describe("get topic data", function() {
-  it("should read dwave topic", function() {
-    const dwaveTopic = getTopic('dwave');
-    console.log(dwaveTopic);
-    // const id = dwave.id
-    // expect(id).to.equal("MDU6VG9waWNkd2F2ZQ==");
+describe("get topic data id", function() {
+  it("should read dwave topic id", function() {
+    const dwave = getTopic('dwave');
+    // console.log(dwave);
+    const id = dwave.id
+    const relatedTopics = dwave.relatedTopics
+    expect(id).to.equal("MDU6VG9waWNkd2F2ZQ==");
+    expect(relatedTopics.length).to.equal(0);
+  });
+});
+
+describe("get related topic data", function() {
+  it("should read libp2p related topics", function() {
+    const libp2p = getTopic('libp2p');
+    // console.log(dwave);
+    const relatedTopics = libp2p.relatedTopics
+    expect(relatedTopics.length).to.equal(2);
   });
 });
