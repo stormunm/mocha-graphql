dbfc706f5cee57867aed30f5cb7eb3f0f20511b6
Michael Angerman <stormasm@gmail.com>
2017-06-21 17:08:50 -0700

    dwave id up and running

--- test/github-data-test.js
+++ test/github-data-test.js
@@ -3,9 +3,9 @@ import { describe, it } from "mocha";
 import TopicMap from "./../src/schema/githubdata/topic";
 
 describe("read topic map", function() {
-  it("should read dwave", function() {
-    console.log(TopicMap);
-    const ok = "1";
-    expect(ok).to.equal("1");
+  it("should read dwave id", function() {
+    const dwave = TopicMap.dwave;
+    const id = dwave.id
+    expect(id).to.equal("MDU6VG9waWNkd2F2ZQ==");
   });
 });
