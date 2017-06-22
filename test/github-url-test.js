16dc3a44aecc41fa24763b561b73f2f2acd47e74
Michael Angerman <stormasm@gmail.com>
2017-06-22 16:44:48 -0700

    cc

--- test/github-url-test.js
+++ test/github-url-test.js
@@ -1,17 +1,20 @@
 import { expect } from "chai";
 import { describe, it } from "mocha";
-import { UniformResourceLocatableMap, getUniformResourceLocatable } from "./../src/schema/githubdata/uniformResourceLocatable";
+import {
+  UniformResourceLocatableMap,
+  getUniformResourceLocatable
+} from "./../src/schema/githubdata/uniformResourceLocatable";
 
 describe("read url map", function() {
   it("should read plum url", function() {
     const plum = UniformResourceLocatableMap.plum;
-    const url = plum.url
+    const url = plum.url;
     //console.log(url);
     expect(url).to.equal("https://github.com/stormasm/plum");
   });
   it("should read dgraph url", function() {
     const dgraph = UniformResourceLocatableMap.dgraph;
-    const url = dgraph.url
+    const url = dgraph.url;
     //console.log(url);
     expect(url).to.equal("https://github.com/dgraph-io/dgraph");
   });
@@ -19,13 +22,13 @@ describe("read url map", function() {
 
 describe("check function getUniformResourceLocatable", function() {
   it("should read function with plum", function() {
-    const plum = getUniformResourceLocatable('plum');
+    const plum = getUniformResourceLocatable("plum");
     //console.log(plum);
     expect(plum.resourcePath).to.equal("/stormasm/plum");
     expect(plum.url).to.equal("https://github.com/stormasm/plum");
   });
   it("should read function with dgraph", function() {
-    const dgraph = getUniformResourceLocatable('dgraph');
+    const dgraph = getUniformResourceLocatable("dgraph");
     //console.log(dgraph);
     expect(dgraph.resourcePath).to.equal("/dgraph-io/dgraph");
     expect(dgraph.url).to.equal("https://github.com/dgraph-io/dgraph");
