e94cdf59d24bdaa27bbf2ca6953bad4a3eded272
Michael Angerman <stormasm@gmail.com>
2017-06-26 11:11:08 -0700

    update repositoryOwner with more data

--- src/schema/githubdata/repositoryOwner.js
+++ src/schema/githubdata/repositoryOwner.js
@@ -18,20 +18,26 @@
 */
 
 const RepositoryOwnerMap = {
-  stormasm: {
-    id: "MDQ6VXNlcjE4MDk5OTE=",
-    login: "stormasm",
-    resourcePath: "/stormasm"
+  "stormasm": {
+      "id": "MDQ6VXNlcjE4MDk5OTE=",
+      "login": "stormasm",
+      "avatarUrl": "https://avatars0.githubusercontent.com/u/1809991?v=3",
+      "resourcePath": "/stormasm",
+      "url": "https://github.com/stormasm"
   },
   "dgraph-io": {
-    id: "MDEyOk9yZ2FuaXphdGlvbjEzOTU4NzA2",
-    login: "dgraph-io",
-    resourcePath: "/dgraph-io"
+      "id": "MDEyOk9yZ2FuaXphdGlvbjEzOTU4NzA2",
+      "login": "dgraph-io",
+      "avatarUrl": "https://avatars3.githubusercontent.com/u/13958706?v=3",
+      "resourcePath": "/dgraph-io",
+      "url": "https://github.com/dgraph-io"
   },
-  graphql: {
-    id: "MDEyOk9yZ2FuaXphdGlvbjEyOTcyMDA2",
-    login: "graphql",
-    resourcePath: "/graphql"
+  "graphql": {
+      "id": "MDEyOk9yZ2FuaXphdGlvbjEyOTcyMDA2",
+      "login": "graphql",
+      "avatarUrl": "https://avatars3.githubusercontent.com/u/12972006?v=3",
+      "resourcePath": "/graphql",
+      "url": "https://github.com/graphql"
   }
 };
 
