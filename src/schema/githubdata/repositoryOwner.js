2e0e31f45d7e682c53894792ccc83ea2bbf7d562
Michael Angerman <stormasm@gmail.com>
2017-06-26 11:46:33 -0700

    pp

--- src/schema/githubdata/repositoryOwner.js
+++ src/schema/githubdata/repositoryOwner.js
@@ -22,36 +22,36 @@
 */
 
 export const RepositoryOwnerMap = {
-  "stormasm": {
-      "id": "MDQ6VXNlcjE4MDk5OTE=",
-      "login": "stormasm",
-      "avatarUrl": "https://avatars0.githubusercontent.com/u/1809991?v=3",
-      "resourcePath": "/stormasm",
-      "url": "https://github.com/stormasm"
+  stormasm: {
+    id: "MDQ6VXNlcjE4MDk5OTE=",
+    login: "stormasm",
+    avatarUrl: "https://avatars0.githubusercontent.com/u/1809991?v=3",
+    resourcePath: "/stormasm",
+    url: "https://github.com/stormasm"
   },
   "dgraph-io": {
-      "id": "MDEyOk9yZ2FuaXphdGlvbjEzOTU4NzA2",
-      "login": "dgraph-io",
-      "avatarUrl": "https://avatars3.githubusercontent.com/u/13958706?v=3",
-      "resourcePath": "/dgraph-io",
-      "url": "https://github.com/dgraph-io"
+    id: "MDEyOk9yZ2FuaXphdGlvbjEzOTU4NzA2",
+    login: "dgraph-io",
+    avatarUrl: "https://avatars3.githubusercontent.com/u/13958706?v=3",
+    resourcePath: "/dgraph-io",
+    url: "https://github.com/dgraph-io"
   },
-  "graphql": {
-      "id": "MDEyOk9yZ2FuaXphdGlvbjEyOTcyMDA2",
-      "login": "graphql",
-      "avatarUrl": "https://avatars3.githubusercontent.com/u/12972006?v=3",
-      "resourcePath": "/graphql",
-      "url": "https://github.com/graphql"
+  graphql: {
+    id: "MDEyOk9yZ2FuaXphdGlvbjEyOTcyMDA2",
+    login: "graphql",
+    avatarUrl: "https://avatars3.githubusercontent.com/u/12972006?v=3",
+    resourcePath: "/graphql",
+    url: "https://github.com/graphql"
   }
 };
 
 export type RepositoryOwner = {
-  type: 'RepositoryOwner',
+  type: "RepositoryOwner",
   id: string,
   login: string,
   avatarUrl: string,
   resourcePath: string,
-  url: string,
+  url: string
 };
 
 /**
--- test/github-repositoryowner-test.js
+++ test/github-repositoryowner-test.js
@@ -1,28 +1,31 @@
 import { expect } from "chai";
 import { describe, it } from "mocha";
-import { RepositoryOwnerMap, getRepositoryOwner } from "./../src/schema/githubdata/repositoryOwner";
-
+import {
+  RepositoryOwnerMap,
+  getRepositoryOwner
+} from "./../src/schema/githubdata/repositoryOwner";
 
 describe("read repository owner map", function() {
   it("should read stormasm login", function() {
     const stormasm = RepositoryOwnerMap.stormasm;
-    const login = stormasm.login
-    const id = stormasm.id
-    const resourcePath = stormasm.resourcePath
+    const login = stormasm.login;
+    const id = stormasm.id;
+    const resourcePath = stormasm.resourcePath;
     expect(login).to.equal("stormasm");
     expect(id).to.equal("MDQ6VXNlcjE4MDk5OTE=");
     expect(resourcePath).to.equal("/stormasm");
   });
 });
 
-
 describe("get repositoryowner data from function", function() {
   it("should read dgraph-io avatar url", function() {
-    const dgraph = getRepositoryOwner('dgraph-io');
+    const dgraph = getRepositoryOwner("dgraph-io");
     //console.log(dgraph);
-    const id = dgraph.id
-    const avatarUrl = dgraph.avatarUrl
+    const id = dgraph.id;
+    const avatarUrl = dgraph.avatarUrl;
     expect(id).to.equal("MDEyOk9yZ2FuaXphdGlvbjEzOTU4NzA2");
-    expect(avatarUrl).to.equal("https://avatars3.githubusercontent.com/u/13958706?v=3");
+    expect(avatarUrl).to.equal(
+      "https://avatars3.githubusercontent.com/u/13958706?v=3"
+    );
   });
 });
